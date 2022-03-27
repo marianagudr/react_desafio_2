a163d22e3d953a686daf7221330501c7ba133b8c
Mariana Guevara <marianagudr@gmail.com>
2022-03-27 14:05:00 -0300

    Primera carga de archivos

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+const Body = () => {
+    return (
+        <div className="Body">
+            <h3>Bienvenid@... Preparate para tener una casa más linda</h3>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import logo from './assets/peppermint_deco_logo_pag.png'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <div>
+                        <a className="navbar-brand" href="#">
+                            <img className="img-fluid" src={logo} width="200" height="auto"/>
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse container-fluid d-flex justify-content-center" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Nosotros</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Productos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
